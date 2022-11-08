488617fb85497cfcf163cfd816a0b24812469c44
Alena Bartosh <alena.mathematics@gmail.com>
2022-11-08 21:40:06 +0200

    🚧 Implement base Board

---
+++ src/board.py
@@ -0,0 +1,14 @@
+from src.cells import CellsType
+
+
+class Board:
+    def __init__(self, height: int, weight: int) -> None:
+        self.height = height
+        self.weight = weight
+
+        if self.height <= 0 or self.weight <= 0:
+            raise ValueError(f"Board parameters are invalid "
+                             f"[height=({self.height})][weight=({self.weight})]!")
+
+        self.__cells: CellsType = \
+            [[False for _ in range(0, self.weight)] for _ in range(0, self.height)]
