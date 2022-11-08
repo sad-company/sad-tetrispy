04e438c4953c68949ff6505f83f5ec6f42acf55c
Dima Pristupa <dima.pristupa@pm.me>
2022-11-09 00:42:41 +0200

    🚧 EngineEventHandler: implement

--- src/base_engine_event_handler.py
+++ src/base_engine_event_handler.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 
+from board import Board
 from engine_event import EngineEvent
 from game_event import GameEvent
 
 
 class BaseEngineEventHandler(ABC):
-    def __init__(self, stdscr):
+    def __init__(self, stdscr, board: Board):
         self._stdscr = stdscr
+        self._board = board
 
     @abstractmethod
     def handle(self, event: EngineEvent) -> GameEvent:
