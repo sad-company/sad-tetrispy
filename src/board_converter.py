ed8337be0ac544040b966535f5bcd78df1ce20ab
onehayzent <sergbartosh@gmail.com>
2022-11-09 15:35:46 +0200

    🚧 board_converter.py from_string()

--- src/board_converter.py
+++ src/board_converter.py
@@ -1,8 +1,8 @@
 from board import Board
+from point import Point
 
 
 class BoardConverter:
-
     @staticmethod
     def to_string(board: Board) -> str:
         result = ''
@@ -11,10 +11,26 @@ class BoardConverter:
             for cell in line:
                 result += '1 ' if cell else '0 '
 
+            result = result.rstrip(' ')
             result += '\n'
 
         return result
 
     @staticmethod
     def from_string(value: str) -> Board:
-        pass
+        lines: list[list[str]] = [line.split(' ')
+                                  for line in value.split('\n')
+                                  if len(line) > 0]
+        height = len(lines)
+        weight = len(lines[0])
+        board = Board(height, weight)
+        points: list[Point] = []
+
+        for x, line in enumerate(lines):
+            for y, cell in enumerate(line):
+                if cell == '1':
+                    points.append(Point(x, y))
+
+        board.set_cells_with_value(points, True)
+
+        return board
