8a636006966bf083e2b945e4942735251eb359c4
Alena Bartosh <alena.mathematics@gmail.com>
2022-11-09 13:47:09 +0200

    🚧 FigureFactory: Implement

---
+++ src/figure_factory.py
@@ -0,0 +1,19 @@
+from figure import Figure
+from figure_kind import FigureKind
+from point import Point
+
+
+class FigureFactory:
+    kind_figure_points_mapping: dict[FigureKind, list[Point]] = {
+        FigureKind.SQUARE: [Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)],
+        FigureKind.LINE: [Point(0, 0), Point(0, 1), Point(0, 2), Point(0, 3)],
+        FigureKind.SNAKE_RIGHT: [Point(0, 1), Point(0, 2), Point(1, 0), Point(1, 1)],
+        FigureKind.SNAKE_LEFT: [Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 2)],
+        FigureKind.CORNER_RIGHT: [Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1)],
+        FigureKind.CORNER_LEFT: [Point(0, 1), Point(1, 1), Point(2, 0), Point(2, 1)],
+        FigureKind.T_LETTER: [Point(0, 0), Point(0, 1), Point(0, 2), Point(1, 1)],
+    }
+
+    @staticmethod
+    def create(cls, kind: FigureKind) -> Figure:
+        return Figure(kind, Point(0, 0), cls.kind_figure_points_mapping[kind])
