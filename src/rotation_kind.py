1ff6654021b70bc5bf2abf8d51f8ad5ecd0d98ad
Alena Bartosh <alena.mathematics@gmail.com>
2022-11-09 23:01:58 +0200

    🚧 RotationKind: get_next(), get_prev()

--- src/rotation_kind.py
+++ src/rotation_kind.py
@@ -1,8 +1,22 @@
-from enum import Enum, auto
+from enum import Enum
 
 
 class RotationKind(Enum):
-    ROTATION_0 = auto()
-    ROTATION_90 = auto()
-    ROTATION_180 = auto()
-    ROTATION_270 = auto()
+    ROTATION_0 = 1
+    ROTATION_90 = 2
+    ROTATION_180 = 3
+    ROTATION_270 = 4
+
+    def get_next(self) -> 'RotationKind':
+        value = self.value + 1
+        if value > 4:
+            return RotationKind.ROTATION_0
+
+        return RotationKind(value)
+
+    def get_prev(self) -> 'RotationKind':
+        value = self.value - 1
+        if value == 0:
+            return RotationKind.ROTATION_270
+
+        return RotationKind(value)
