4a774180beed94e94226c9bf4dfbb109ffd2c171
onehayzent <sergbartosh@gmail.com>
2022-11-10 13:51:35 +0200

    🚧 test_BoardConverter.py test_when_double_convertation()

--- tests/test_BoardConverter.py
+++ tests/test_BoardConverter.py
@@ -21,5 +21,32 @@ class BoardConverterTestCase(unittest.TestCase):
         self.assertEqual(expected, BoardConverter.to_string(board))
 
     def test_convert_from_string_when_correct_cells_line(self):
-        # TODO(AB): Task for Onehayzent
-        pass
+        board_str = '0 0 0 0 0\n' \
+                    '0 1 0 1 0\n' \
+                    '0 0 0 0 0\n' \
+                    '1 0 0 0 1\n' \
+                    '0 1 1 1 0\n' \
+                    '0 0 0 0 0\n'
+
+        expected = Board(6, 5)
+        points = [Point(1, 1), Point(1, 3),
+                  Point(3, 0), Point(3, 4),
+                  Point(4, 1), Point(4, 2),
+                  Point(4, 3)]
+
+        expected.set_cells_with_value(points, True)
+
+        self.assertEqual(expected.get_cells(), BoardConverter.from_string(board_str).get_cells())
+
+    def test_when_double_convertation(self):
+        board_srt = '0 0 0 0 0\n' \
+                    '0 1 0 1 0\n' \
+                    '0 0 0 0 0\n' \
+                    '1 0 0 0 1\n' \
+                    '0 1 1 1 0\n' \
+                    '0 0 0 0 0\n'
+
+        board_from_str = BoardConverter.from_string(board_srt)
+        board_from_str = BoardConverter.to_string(board_from_str)
+
+        self.assertEqual(board_srt, board_from_str)
