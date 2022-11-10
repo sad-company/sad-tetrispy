ca4445e98b5d9f3a10d768c003c70fe6fa461383
Alena Bartosh <alena.mathematics@gmail.com>
2022-11-10 11:55:14 +0200

    ✅ BoardConverterTestCase.test_convert_to_string()

---
+++ tests/test_BoardConverter.py
@@ -0,0 +1,23 @@
+import unittest
+
+from board import Board
+from board_converter import BoardConverter
+from point import Point
+
+
+class BoardConverterTestCase(unittest.TestCase):
+    def test_convert_to_string(self):
+        board = Board(3, 4)
+        points = [Point(1, 2), Point(1, 3), Point(2, 0), Point(2, 1), Point(2, 2), Point(2, 3)]
+        board.set_cells_with_value(points, True)
+        # ----
+        # --xx
+        # xxxx
+
+        expected = '0 0 0 0\n0 0 1 1\n1 1 1 1\n'
+
+        self.assertEqual(expected, BoardConverter.to_string(board))
+
+    def test_convert_from_string_when_correct_cells_line(self):
+        # TODO(AB): Task for Serg
+        pass
