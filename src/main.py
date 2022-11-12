d4c871b23b21ec2d9f7e4515989a318547ba7031
Dima Pristupa <dima.pristupa@pm.me>
2022-11-12 13:04:42 +0200

    🚧 Remove prefilled_points

--- src/main.py
+++ src/main.py
@@ -3,17 +3,10 @@ import curses
 from board import Board
 from engine import Engine
 from engine_event_handler import EngineEventHandler
-from point import Point
 
 
 def main(stdscr) -> None:
     board = Board(height=20, weight=10)
-    # TODO(DP): --> remove after test
-    prefilled_points = [Point(board.height - 1, i) for i in range(5)]
-
-    board.set_cells_with_value(prefilled_points, True)
-    # TODO(DP): <-- remove after test
-
     event_handler = EngineEventHandler(stdscr, board)
     engine = Engine(stdscr, event_handler, tick_duration_in_sec=1)
 
