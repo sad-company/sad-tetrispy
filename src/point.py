3316e6e912aec80d46f8907bfa37d771d1476b3d
Dima Pristupa <dima.pristupa@pm.me>
2022-11-12 13:05:17 +0200

    🚧 Point.__add__: implement

--- src/point.py
+++ src/point.py
@@ -6,6 +6,10 @@ class Point:
     x: int
     y: int
 
+    def __add__(self, other):
+        return Point(self.x + other.x,
+                     self.y + other.y)
+
     def is_in_board(self, height: int, weight: int) -> bool:
         is_x_in_bounds = 0 <= self.x < height
         is_y_in_bounds = 0 <= self.y < weight
