82ebbf3287b08ef4198af98ce4617a5f5edeb6e7
Dima Pristupa <dima.pristupa@pm.me>
2022-11-14 17:11:38 +0200

    ♻️ EngineEventHandler: extract handle_*()

--- src/engine_event_handler.py
+++ src/engine_event_handler.py
@@ -25,6 +25,41 @@ class EngineEventHandler(BaseEngineEventHandler):
     def __is_game_end(self) -> bool:
         return not self._board.is_cells_empty(self.__current_figure.get_points())
 
+    def __handle_figure_move_right(self) -> None:
+        new_points, new_position = FigureMover.move_right(self.__current_figure)
+
+        if self._board.is_cells_empty(new_points):
+            self.__current_figure.position = new_position
+
+    def __handle_figure_move_left(self) -> None:
+        new_points, new_position = FigureMover.move_left(self.__current_figure)
+
+        if self._board.is_cells_empty(new_points):
+            self.__current_figure.position = new_position
+
+    def __handle_figure_move_down(self) -> None:
+        new_points, new_position = FigureMover.move_down(self.__current_figure)
+
+        if self._board.is_cells_empty(new_points):
+            self.__current_figure.position = new_position
+        else:
+            self._board.set_cells_with_value(self.__current_figure.get_points(), True)
+
+            removed_lines = self._board.remove_lines_if_needed()
+
+            if removed_lines > 0:
+                self.__score_holder.increment(removed_lines)
+
+                self.__use_next_figure()
+
+    def __handle_figure_rotate(self) -> None:
+        new_points, new_rotation = FigureMover.rotate(self.__current_figure)
+        points_for_check = self.__current_figure.position.add_to(new_points)
+
+        if self._board.is_cells_empty(points_for_check):
+            self.__current_figure.points = new_points
+            self.__current_figure.rotation = new_rotation
+
     def handle(self, event: EngineEvent) -> GameEvent:
         if self.__is_game_end():
             return GameEvent.END
@@ -35,40 +70,17 @@ class EngineEventHandler(BaseEngineEventHandler):
         if self.__is_paused:
             return GameEvent.CONTINUE
 
-        elif event == EngineEvent.TIME_TICK:
-            new_points, new_position = FigureMover.move_down(self.__current_figure)
-
-            if self._board.is_cells_empty(new_points):
-                self.__current_figure.position = new_position
-            else:
-                self._board.set_cells_with_value(self.__current_figure.get_points(), True)
-
-                removed_lines = self._board.remove_lines_if_needed()
-
-                if removed_lines > 0:
-                    self.__score_holder.increment(removed_lines)
-
-                self.__use_next_figure()
+        if event == EngineEvent.TIME_TICK:
+            self.__handle_figure_move_down()
 
         elif event == EngineEvent.MOVE_RIGHT:
-            new_points, new_position = FigureMover.move_right(self.__current_figure)
-
-            if self._board.is_cells_empty(new_points):
-                self.__current_figure.position = new_position
+            self.__handle_figure_move_right()
 
         elif event == EngineEvent.MOVE_LEFT:
-            new_points, new_position = FigureMover.move_left(self.__current_figure)
-
-            if self._board.is_cells_empty(new_points):
-                self.__current_figure.position = new_position
+            self.__handle_figure_move_left()
 
         elif event == EngineEvent.ROTATE_CLOCKWISE:
-            new_points, new_rotation = FigureMover.rotate(self.__current_figure)
-            points_for_check = self.__current_figure.position.add_to(new_points)
-
-            if self._board.is_cells_empty(points_for_check):
-                self.__current_figure.points = new_points
-                self.__current_figure.rotation = new_rotation
+            self.__handle_figure_rotate()
 
         self.__renderer.render(self._board,
                                self.__current_figure,
