73f1862887ea391c5b52b1c81915130bf921f5af
Dima Pristupa <dima.pristupa@pm.me>
2022-11-14 17:56:44 +0200

    ✨ Increase game speed based on removed lines

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ from engine_event_handler import EngineEventHandler
 
 def main(stdscr) -> None:
     board = Board(height=20, weight=10)
-    event_handler = EngineEventHandler(stdscr, board)
-    engine = Engine(stdscr, event_handler, tick_duration_in_sec=1)
+    event_handler = EngineEventHandler(stdscr,
+                                       board,
+                                       initial_ticks_for_move_down=50)
+    engine = Engine(stdscr, event_handler, tick_duration_in_sec=0.02)
 
     engine.run()
 
