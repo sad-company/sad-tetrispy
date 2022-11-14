5d763df6ef033e1f7cf01a11f2d87e7d9d87df88
onehayzent <sergbartosh@gmail.com>
2022-11-14 23:48:28 +0200

    ✏️ Fix review comments

--- src/figure_factory.py
+++ src/figure_factory.py
@@ -10,14 +10,15 @@ class FigureFactory:
     __kind_randomizer = FigureKindRandomizer()
 
     @staticmethod
-    def create(kind: FigureKind, weight: int) -> Figure:
+    def create(kind: FigureKind, board_weight: int) -> Figure:
         start_rotation = RotationKind.ROTATION_0
         figure_points = FigureMover.get_points_after_rotation(kind, start_rotation)
+        center_position = board_weight // 2 - 1
 
-        return Figure(kind, Point(0, weight // 2 - 2), figure_points, start_rotation)
+        return Figure(kind, Point(0, center_position), figure_points, start_rotation)
 
     @staticmethod
-    def create_random(weight: int) -> Figure:
+    def create_random(board_weight: int) -> Figure:
         random_kind = FigureFactory.__kind_randomizer.get_random()
 
-        return FigureFactory.create(random_kind, weight)
+        return FigureFactory.create(random_kind, board_weight)
